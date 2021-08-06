d03da8a60f273873a9bfea85835900bcbb1f3219
yoshikawagc <yoshikawa.gaku.gc@gmail.com>
2021-08-06 18:59:24 +0900

    fix

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const requestWetherApi = async () => {
-	const queryCity = "tokyo";
-	const queryCount = 50;
-	const queryMode = null;//NULL(json) or XML or HTML
-	const url = `https://community-open-weather-map.p.rapidapi.com/find?q=${queryCity}&cnt=${queryCount}&mode=${queryMode}&lon=0&type=link%2C%20accurate&lat=0&units=imperial%2C%20metric`;
+	const query = {
+		"city": "tokyo",
+		"count": 50,
+		"mode": null, //NULL(json) or XML or HTML
+	}
+	const url = `https://community-open-weather-map.p.rapidapi.com/find?q=${query.city}&cnt=${query.count}&mode=${query.mode}&lon=0&type=link%2C%20accurate&lat=0&units=imperial%2C%20metric`;
 	const option =  {
 		"method": "GET",
 		"headers": {
@@ -12,7 +14,11 @@ const requestWetherApi = async () => {
 	}
 	const response = await fetch(url, option);
 	const json = await response.json()
-	console.log(json)
+	const weather = json.list[0].weather;
+
+	for (const item of weather) {
+		console.log(item)
+	}
 }
 
 requestWetherApi()
